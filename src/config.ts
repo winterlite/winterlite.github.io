69973948efd4a4746019dd28afa19b9d3bbc61d6
mjruiz <winterlite@icloud.com>
2026-04-28 14:06:35 +0900

    i broke type

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: false,
+    enabled: true,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
